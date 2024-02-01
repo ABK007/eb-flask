bd57a392b4418108134320912fe582a0b3a3fb86
ABK007 <abubakarzzz07@gmail.com>
2024-02-01 07:31:17 +0500

    Added code in routes.py to integrate AWS SNS service with the app. The email will be received at the given email address on every form submission.

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,13 @@ from flask import Flask, render_template, redirect, url_for, flash
 from app.forms import SignUpForm
 import boto3
 
+# dynamodb
 db = boto3.resource('dynamodb', region_name='us-west-2')
 table = db.Table('signuptable')
+
+# AWS SNS
+notification = boto3.client('sns', region_name='us-west-2')
+topic_arn = "arn:aws:sns:us-west-2:936668792450:eb-flask-sns"
 
 
 @application.route('/')
@@ -14,7 +19,7 @@ def home_page():
     return render_template('home.html')
 
 
-@application.route('/signup', methods = ['GET', 'POST'])
+@application.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     form = SignUpForm()
     if form.validate_on_submit():
@@ -30,6 +35,13 @@ def sign_up():
         msg = 'Congratulations !!! {} is now a premium member !'.format(form.name.data)
         flash(msg)
 
+        # sending email to owner using sns
+        email_message = f"\nname: {form.name.data}" \
+                        f"\nemail: {form.email.data}" \
+                        f"\nmobile: {form.mobile.data}" \
+                        f"\ncountry: {form.country.data}"
+
+        notification.publish(Message=email_message, TopicArn=topic_arn, Subject="A new signup form submitted")
         return redirect(url_for('home_page'))
 
     return render_template('signup.html', form=form)
